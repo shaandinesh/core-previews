e3923fd95266a3f623319b9da053fbb4a053ad9e
karrie <karrie@streamlit.io>
2020-10-27 14:13:38 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,16 @@ import numpy as np
 import pandas as pd
 import streamlit as st
 
+execbox(	
+    """	
 cols = st.number_input("Number of columns", value=5)
 df = pd.DataFrame(np.random.randint(0, 10, size=(10, cols)))
 st.dataframe(df)
+""",	
+    autorun=True,	
+    line_numbers=True,	
+    height=300,	
+)
+
+
+
