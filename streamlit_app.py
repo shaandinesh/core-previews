61fa3d0a1ab3c9ce85ab9b40018e39a0a7341f39
Henrikh Kantuni <henrikh.kantuni@gmail.com>
2020-12-08 17:38:06 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,10 @@ import streamlit as st
 
 st.title("Slider")
 
-st.slider("Label 1", 0, 100, 25, 1)
+v1 = st.slider("Label 1", 0, 100, 25, 1)
+st.write("Value:", v1)
+
+st.title("Multiselect")
+
+v2 = st.multiselect("selectbox 4", ["coffee", "tea", "water"], ["tea", "water"])
+st.text("Value: %s" % v2)
