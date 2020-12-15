608d72fa9cb10396c721d3a4a5f73f45c9216625
karrie <karrie@streamlit.io>
2020-12-15 00:17:01 -0500

    streamlit_player demo

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,10 +23,35 @@ if selected_script:
 else:
     script = """
 import streamlit as st
+from pathlib import Path
+from streamlit_player import st_player, _SUPPORTED_EVENTS
 
-st.header("Results")
-if st.button("Press me!"):
-    st.balloons()
+with st.sidebar:
+    "## ⚙️ Parameters"
+
+    options = {
+        "events": st.multiselect("Events to listen", _SUPPORTED_EVENTS, ["onProgress"]),
+        "progress_interval": st.slider("Progress refresh interval (ms)", 200, 2000, 500, 1),
+        "volume": st.slider("Volume", 0.0, 1.0, 1.0, .01),
+        "playing": st.checkbox("Playing", False),
+        "loop": st.checkbox("Loop", False),
+        "controls": st.checkbox("Controls", True),
+        "muted": st.checkbox("Muted", True),
+        "play_inline": st.checkbox("Play inline", False),
+    }
+
+c1, c2 = st.beta_columns(2)
+
+with c1:
+    url = st.text_input("First URL", "https://youtu.be/CmSKVW1v0xM")
+    event = st_player(url, **options, key=1)
+    event
+
+with c2:
+    url = st.text_input("Second URL", "https://soundcloud.com/imaginedragons/demons")
+    event = st_player(url, **options, key=2)
+    event
+
     """
 
 st.header("Edit my source 👇")
